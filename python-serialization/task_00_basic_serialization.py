1b558b3a6eaa1dab67c33fcde305dfa5e2fb20b3
splint314 <krigal323@gmail.com>
2026-05-27 14:31:53 +0200

    added informations datas

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -5,12 +5,33 @@
 import json
 
 
-def to_json_string(my_obj):
+def serialize_and_save_to_file(data, filename):
+    """Serializes an object to a JSON file.
 
-    """Returns the JSON representation of an object (string)"""
-    return json.dumps(my_obj)
+    Args:
+    data: The object to serialize.
+    filename: The name of the file to save the JSON string to.
+    """
+    with open(filename, 'w') as f:
+        json.dump(data, f)
 
-    if __name__ == "__main__":
-        my_obj = [1, 2, 3, 4]
-    json_string = to_json_string(my_obj)
-    print(json_string)
+
+def load_and_deserialize(filename):
+
+    """Deserializes a JSON file to an object.
+
+    Args:
+    filename: The name of the file to load the JSON string from.
+
+    Returns:
+    The deserialized object.
+    """
+    with open(filename, 'r') as f:
+        return json.load(f)
+
+
+if __name__ == "__main__":
+    my_data = {'name': 'Alice', 'age': 30, 'city': 'New York'}
+    serialize_and_save_to_file(my_data, 'data.json')
+    loaded_data = load_and_deserialize('data.json')
+    print(loaded_data)
