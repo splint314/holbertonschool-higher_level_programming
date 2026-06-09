5e39bd34628a14ca3fad8c127e16618631d4ea4d
splint314 <krigal323@gmail.com>
2026-06-09 17:51:10 +0200

    fix: streamline query format execution

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -16,11 +16,8 @@ if __name__ == "__main__":
         db=argv[3]
     )
     cur = db.cursor()
-    cur.execute(
-        "SELECT * FROM states "
-        "WHERE name LIKE BINARY '{}' "
-        "ORDER BY id ASC".format(argv[4])
-    )
+    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC"
+    cur.execute(query.format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
