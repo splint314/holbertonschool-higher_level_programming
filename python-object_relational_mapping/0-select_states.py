139b55d457139db4eb16b45a1fa9877e9d4495ea
splint314 <krigal323@gmail.com>
2026-06-09 16:45:14 +0200

    fix: chmod

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+
 """
 This script lists all states from the database hbtn_0e_0_usa.
 It takes 3 arguments: mysql username, mysql password and database name.
 """
+
 import MySQLdb
 from sys import argv
 
