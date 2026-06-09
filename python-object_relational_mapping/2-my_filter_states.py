a8fff7c0507feefb41d390f51bda8dbec3ac5eab
splint314 <krigal323@gmail.com>
2026-06-09 17:49:08 +0200

    feat: filtered states by using user input with case matching

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This script takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument.
+"""
+import MySQLdb
+from sys import argv
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+    cur = db.cursor()
+    cur.execute(
+        "SELECT * FROM states "
+        "WHERE name LIKE BINARY '{}' "
+        "ORDER BY id ASC".format(argv[4])
+    )
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
