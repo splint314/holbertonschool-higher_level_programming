f8c7c7849d104792ef11e22c631a6ecfdf6d2742
splint314 <krigal323@gmail.com>
2026-06-09 17:55:55 +0200

    fix: state filter query

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 This script takes in an argument and displays all values in the states table
-of hbtn_0e_0_usa where name matches the argument.
+where name matches the argument.
 """
 import MySQLdb
 from sys import argv
@@ -15,11 +15,16 @@ if __name__ == "__main__":
         passwd=argv[2],
         db=argv[3]
     )
+
     cur = db.cursor()
-    query = "SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC"
+
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC"
     cur.execute(query.format(argv[4]))
+
     rows = cur.fetchall()
+
     for row in rows:
         print(row)
+
     cur.close()
     db.close()
