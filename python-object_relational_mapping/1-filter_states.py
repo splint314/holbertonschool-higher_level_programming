260c8109ed4e9c0e18bd3bed2efff307f0a38cf9
splint314 <krigal323@gmail.com>
2026-06-09 17:34:24 +0200

    fix: handle NULL values and strict upper N case

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
-        if row[1][0] == 'N':
+        if row[1] and row[1][0] == 'N':
             print(row)
     cur.close()
     db.close()
