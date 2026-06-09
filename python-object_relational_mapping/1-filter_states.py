40a523375402d1d2714661a27726d7c52f576380
splint314 <krigal323@gmail.com>
2026-06-09 17:10:04 +0200

    fix: blank line

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -18,10 +18,10 @@ if __name__ == "__main__":
     )
     cur = db.cursor()
     cur.execute(
-        "SELECT * FROM states"
+        "SELECT * FROM states "
         "WHERE name LIKE BINARY 'N%' "
         "ORDER BY id ASC"
-        )
+    )
     rows = cur.fetchall()
     for row in rows:
         print(row)
