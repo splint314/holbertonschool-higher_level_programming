2131066ea3cdbf8b458c898c8ebbf55a4f2037f0
splint314 <krigal323@gmail.com>
2026-07-01 17:19:16 +0200

    fix: semistandard

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
 if (isNaN(process.argv[2])) {
-	console.log('NaN');
+  console.log('NaN');
 } else {
-	const a = parseInt(process.argv[2]);
-	const b = parseInt(process.argv[3]);
-	console.log(a + b);
+  const a = parseInt(process.argv[2]);
+  const b = parseInt(process.argv[3]);
+  console.log(a + b);
 }
