bcd09d5ea9631f4fe7cd41dcd169338dfa62788e
splint314 <krigal323@gmail.com>
2026-07-02 15:36:23 +0200

    feat: toggled a class of header

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,4 @@
+document.querySelector('#toggle_header').addEventListener('click', () => {
+  document.querySelector('header').classList.toggle('red');
+  document.querySelector('header').classList.toggle('green');
+});
