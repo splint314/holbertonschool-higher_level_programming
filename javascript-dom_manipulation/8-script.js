d8b7bbd470c65ef39ce9bc4662e04fd1251f9b5f
splint314 <krigal323@gmail.com>
2026-07-03 14:11:12 +0200

    feat: fetched and displayed a value

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,6 @@
+fetch('https://hellosalut.stefanbohacek.com/?lang=fr')
+  .then((response) => response.json())
+  .then((data) => {
+    document.querySelector('#hello').textContent = data.hello;
+  });
+  
